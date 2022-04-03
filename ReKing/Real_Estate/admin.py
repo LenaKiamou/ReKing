f3751bae0ab9d5393afacdc270e17bf606806d62
Xenofwntas <sarantopoulos.john@yahoo.gr>
2022-04-03 23:19:01 +0300

    Updated Urls / views.py

--- ReKing/Real_Estate/admin.py
+++ ReKing/Real_Estate/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Features)
+
+class ImagesAdmin(admin.StackedInline):
+    model = Images
+
+@admin.register(House)
+class HouseAdmin(admin.ModelAdmin):
+    inlines = [ImagesAdmin]
+
+    class Meta:
+        model = House
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    pass
